c4831612a9e3208d05b632275a843f513b341a90
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2019-09-10 00:45:40 -0500

    Begin authorization code.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,3 +2,19 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypts");
 
+const createToken = require("../token/tokenCoin"),createToken;
+const Users = require("../models/user-model");
+
+//Still need to figure out rest router
+
+//need to generate password, user and ERRORS AS WELL AS LOGIN
+
+// router.post("api/register", (req, res) => {
+//     const user = req.body;
+
+//     if(!user.username || !user.password) {
+//         res.status(400).json({errorMessage: "Please provide correct login info."});
+//     } else {
+
+//     }
+// } )
