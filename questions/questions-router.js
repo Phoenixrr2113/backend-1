95bf10376bd67184e49d6ee4e5b1cfa27718b1f7
Scott Schindler <scottschindler29@gmail.com>
2019-09-15 19:57:44 -0400

    config changes for deployment

--- questions/questions-router.js
+++ questions/questions-router.js
@@ -1,10 +1,25 @@
-const express = require("express");
-const db = require("./data/db-config.js");
-const server = express();
-server.use(express.json());
+const router = require("express").Router();
+const Questions = require("../models/questions-model.js");
+const db = require("../database/dbConfig.js");
+
+// post question
+router.post("/", async (req, res) => {
+  try {
+    const [id] = await db("questions").insert(req.body);
+
+    const question = await db("questions")
+      .where({ id })
+      .first();
+
+    res.status(201).json(question);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 
 //get questions
-server.get("/api/questions", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const questions = await db("questions");
     res.status(200).json(questions);
@@ -14,7 +29,7 @@ server.get("/api/questions", async (req, res) => {
 });
 
 // GET a question by id
-server.get("/api/questions/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const [question] = await db("questions").where({ id });
@@ -34,19 +49,38 @@ server.get("/api/questions/:id", async (req, res) => {
   }
 });
 
-// post question
-server.post("/api/questions", async (req, res) => {
+// PUT "/api/questions/:id" - edits a question from the Database. - make sure the user can only edit their own questions.
+router.put("/:id", async (req, res) => {
+  console.log("endpoint hit...");
   try {
-    const [id] = await db("questions").insert(req.body);
-
-    const question = await db("questions")
-      .where({ id })
-      .first();
-
-    res.status(201).json(question);
+    const question = await Questions.update(req.params.id, req.body);
+    if (question) {
+      res.status(200).json({ status: "Question has been updated" });
+    } else {
+      res.status(404).json({ message: "The question could not be found" });
+    }
   } catch (error) {
     console.log(error);
-    res.status(500).json(error);
+    res.status(500).json({
+      message: "Error updating the question"
+    });
+  }
+});
+
+// DELETE "/api/question/:id" - deletes a question from the Database. - make sure the user can only delete their own questions.
+router.delete("/:id", async (req, res) => {
+  try {
+    const question = await Questions.remove(req.params.id);
+    if (question > 0) {
+      res.status(200).json({ Message: "The question has been deleted" });
+    } else {
+      res.status(404).json({ message: "the question could not be found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error removing the Question"
+    });
   }
 });
 
