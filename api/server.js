cfbc4bcd0f0ce7dc1754af52a788e9185f18b5b6
Scott Schindler <scottschindler29@gmail.com>
2019-09-15 16:26:50 -0400

    Revert "Brandy rimes"

--- api/server.js
+++ api/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
-//listening on 5000
+
 const port = 5000;
 
 const server = express();
 
 server.use(express.json());
-//outputs the WELCOME
+
 server.get("/", (req, res) => {
-    res.send("Welcome to Advice Giver!");
+  res.send("Welcome to Advice Giver!");
 });
-//Makes sure we are listening
+
 server.listen(5000, () => {
-    console.log("Server listening on port ${port}");
+  console.log("Server listening on port ${port}");
 });
